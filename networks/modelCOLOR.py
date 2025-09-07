9bca42768b3d7eba154793ca793010621c8958aa
Ali Erdem <aeeksilmez@gmail.com>
2025-09-07 16:05:14 +0300

    v_1

---
+++ networks/modelCOLOR.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Config
+IMG_SIZE = 64
+NUM_CLASSES = 2
+DATA_DIR = "TRAINING"
+
+# Data Generator
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+
+train_gen = datagen.flow_from_directory(
+    DATA_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode="grayscale",
+    batch_size= 20,
+    class_mode="categorical",
+    subset="training"
+)
+
+val_gen = datagen.flow_from_directory(
+    DATA_DIR,
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode="grayscale",
+    batch_size=32,
+    class_mode="categorical",
+    subset="validation"
+)
+
+model = models.Sequential([
+    layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1)),
+    layers.Conv2D(90, (3, 3), activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(180, (3, 3), activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(NUM_CLASSES, activation='softmax')
+])
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+model.fit(train_gen, validation_data=val_gen, epochs=10)
+
+model.save("siyahbeyaz.keras")
